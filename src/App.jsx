d32731fbaaa16179c6cb3e9743ee0df1ee28b1f1
pdro-maker07 <putra.timur804@gmail.com>
2025-09-24 02:02:22 +0800

    ditambahkan

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="p-2 text-center">hell</h1>
+      <p className="font-light">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit ipsum dolorum aliquam illo tempore nesciunt, accusamus maiores, veniam nam velit ab? Labore qui dignissimos eum modi ipsa molestiae? Molestias, eos fugiat! Incidunt atque sequi corporis nobis corrupti, pariatur eveniet ut saepe, autem repudiandae, porro molestias a architecto laboriosam quasi facere deserunt? Tempora, eius et? Labore totam aperiam repellendus delectus, inventore illo. Nihil labore optio fugiat reiciendis non maiores at, eaque praesentium consectetur, impedit repudiandae. A eum itaque quasi qui distinctio? Nam, rerum. Id adipisci illum consequatur totam doloremque beatae quasi esse culpa. Quod vitae ad ducimus rerum quam deserunt voluptatibus excepturi. Provident delectus optio illum velit nobis aliquid porro blanditiis rem culpa ratione officia fuga voluptas eveniet sit sint laboriosam esse, impedit officiis quae voluptates illo soluta. Impedit aperiam saepe dolores nulla non est voluptatem illum? Ratione et vero provident eum velit iste mollitia delectus excepturi maiores aspernatur, harum amet. Repudiandae unde ipsa earum consequuntur asperiores molestias aspernatur harum dolore mollitia, at autem facere ipsam? Nesciunt nobis quas voluptatem tempore quis maxime culpa sed in doloribus enim at, sunt, et deserunt rem aliquid adipisci dignissimos excepturi beatae! Recusandae, fugit, ratione beatae asperiores, est nam minima voluptas error incidunt vero numquam.</p>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./Todolist/index.html",
+    "./Todolist/src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
