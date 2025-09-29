1ba53a3e6e11b33ea4949d5960d2a00590ab660d
pdro-maker07 <putra.timur804@gmail.com>
2025-09-30 03:17:37 +0800

    terbru

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Form from './Form'
 import {v4 as uuidv4} from 'uuid';
+import Todo from "./Todo"
 uuidv4()
 
 
@@ -14,8 +15,13 @@ const TodoList = () => {
   return (
     <div className='container bg-gray-700 mt-20 p-8 w-[600px] rounded-2xl ml-auto mr-auto'>
       <Form createTodo = {createTodo}/>
+      {
+        todoValue.map((todo, idx) => (
+          <Todo task={todo} key={idx} />
+        ))
+      }
     </div>
-  )
-}
+  );
+};
 
 export default TodoList
